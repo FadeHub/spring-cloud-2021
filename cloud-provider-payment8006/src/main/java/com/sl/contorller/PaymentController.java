76f98fe8f37da561a8e53319f10800739260b4fe
shuliangzhao <809894780@qq.com>
2021-06-10 11:13:24 +0800

    注册中心consul 服务注册

---
+++ cloud-provider-payment8006/src/main/java/com/sl/contorller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.sl.contorller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author shuliangzhao
+ * @Title: PaymentController
+ * @ProjectName spring-cloud-2021
+ * @Description: TODO
+ * @date 2021/6/10 11:11
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentConsul()
+    {
+        return "springcloud with consul: "+serverPort+"\t   "+ UUID.randomUUID().toString();
+    }
+}
